8c78c18c8e3f912b1d0211d27c832a5b95fef141
Endrik <endriksan555@gmail.com>
2026-09-08 14:18:35 -0300

    feat: solve exercise 06 Twice Triple and Root

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n = eval(input('Type a number: '))
+
+print('Twice as much as {} is worth {}!'.format(n, (2 * n)))
+print('The triple of {} is worth {}!'.format(n, (3 * n)))
+print('The square root of {} is equals {:.2f}!'.format(n, (pow(n, 0.5))))
